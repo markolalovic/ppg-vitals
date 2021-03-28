9f0969437018b3f390cf93ef6cf6707e50aa8749
Marko Lalovic <marko.lalovic.email@gmail.com>
2021-03-28 21:43:08 +0200

    simplified

--- src/main.js
+++ src/main.js
@@ -1,97 +1,135 @@
 document.querySelector('#record').addEventListener('click', onRecord);
 
 const inProduction = true; // hide video and tmp canvas
-const channel = 'r'; // red by default TODO: could include g, b
+const channel = 'r'; // red only, green='g' and blue='b' channels can be added
 
 let video, c_tmp, ctx_tmp; // video from rear-facing-camera and tmp canvas
 let frameCount = 0; // count number of video frames processed 
 let delay = 0; // delay = 100; should give us 10 fps, estimated around 7
-let burnIn = 50; // skip the initial burnIn number of frames
-const rgbInit = 1.5 - 250.0/255; // expected mean intensity around .6
-let rgbMean = {
-  'r': rgbInit, 
-  'g': rgbInit, 
-  'b': rgbInit
-};
-let rgbMeanArr = []; // save the signal for analysis
 let numOfQualityFrames = 0; // TODO: count the number of quality frames
+let xMeanArr = [];
+let xMean = 0;
+let initTime;
+
+let nFrame = 0;
+const WINDOW_LENGTH = 100;
+let acdc = Array(WINDOW_LENGTH).fill(0.5);
+let ac = Array(WINDOW_LENGTH).fill(0.5);
+
+// draw the signal data as it comes
+let lineArr = [];
+const MAX_LENGTH = 100;
+const DURATION = 100;
+let chart = realTimeLineChart();
 
 let constraintsObj = {
-    audio: false,
-    video: { 
-        // 1MP = 720p = 1280 x 720
-        // 2MP = 1080p = 1920 x 1080
-        // 4MP = 2K = 2688 x 1520
-        maxWidth: 1920,
-        maxHeight: 1080, 
-        frameRate: {ideal: 240 },
-        facingMode: 'environment' // rear-facing-camera
-    }
+  audio: false,
+  video: {
+    maxWidth: 1280,
+    maxHeight: 720,
+    frameRate: { ideal: 240 },
+    facingMode: 'environment' // rear-facing-camera
+  }
 };
 
 function setWH() {
-    let [w, h] = [video.videoWidth, video.videoHeight];
-    document.getElementById('delay').innerHTML = `Frame compute delay: ${delay}`;
-    document.getElementById('resolution').innerHTML = `Video resolution: ${w} x ${h}`;
-    c_tmp.setAttribute('width', w);
-    c_tmp.setAttribute('height', h);    
+  let [w, h] = [video.videoWidth, video.videoHeight];
+  document.getElementById('delay').innerHTML = `Frame compute delay: ${delay}`;
+  document.getElementById('resolution').innerHTML = `Video resolution: ${w} x ${h}`;
+  c_tmp.setAttribute('width', w);
+  c_tmp.setAttribute('height', h);
 }
 
 function init() {
-    c_tmp = document.getElementById('output-canvas');
-    if (inProduction) {
-      c_tmp.style.display = 'none';
-    }
-    ctx_tmp = c_tmp.getContext('2d');
-    startTime = new Date();
+  c_tmp = document.getElementById('output-canvas');
+  if (inProduction) {
+    c_tmp.style.display = 'none';
+  }
+  ctx_tmp = c_tmp.getContext('2d');
 }
 
 function computeFrame() {
-    // if (numOfQualityFrames < ) {
-    ctx_tmp.drawImage(video,
-        0, 0, video.videoWidth, video.videoHeight);
-    let frame = ctx_tmp.getImageData(
-        0, 0, video.videoWidth, video.videoHeight);
+  ctx_tmp.drawImage(video,
+    0, 0, video.videoWidth, video.videoHeight);
+  let frame = ctx_tmp.getImageData(
+    0, 0, video.videoWidth, video.videoHeight);
 
-    // process each frame
-    const count = frame.data.length/4;
-    let rgb = {'r': 0, 'g': 0, 'b': 0};
-    for (let i=0; i < count; i++) {
-        rgb['r'] += frame.data[i * 4 + 0];
-        rgb['g'] += frame.data[i * 4 + 1];
-        rgb['b'] += frame.data[i * 4 + 2];
-    }
-    // TODO: maybe invert to show the PPG signal not inverted signal
-    // abs(max(x)-x)
-    if (frameCount > burnIn) {
-      rgbMean['r'] = 1.5 - rgb['r'] / (count*255);
-      rgbMean['g'] = 1.5 - rgb['g'] / (count*255);
-      rgbMean['b'] = 1.5 - rgb['b'] / (count*255);
-    }
+  // process each frame
+  const count = frame.data.length / 4;
+  let rgbRed = 0;
+  for (let i = 0; i < count; i++) {
+    rgbRed += frame.data[i * 4];
+  }
+  // invert to plot the PPG signal
+  xMean = 1 - rgbRed / (count * 255);
 
-    let rgbMeanData = {
-      time: new Date(),
-      x: rgbMean[channel],
-    };
-    rgbMeanArr.push(rgbMeanData);
+  let xMeanData = {
+    time: (new Date() - initTime) / 1000,
+    x: xMean
+  };
 
-    document.getElementById('frame-time').innerHTML = `Frame time: ${(rgbMeanData.time - startTime) / 1000}`;
-    document.getElementById('video-time').innerHTML = `Video time: ${(video.currentTime)}`;
-    document.getElementById('signal').innerHTML = `X: ${rgbMeanData.x}`;
+  acdc[nFrame % WINDOW_LENGTH] = xMean;
 
-    const fps = (++frameCount / video.currentTime).toFixed(3); 
-    document.getElementById('frame-fps').innerHTML = `Frame count: ${frameCount}, FPS: ${fps}`;
+  // TODO: calculate AC from AC-DC only each WINDOW_LENGTH time:
+  if (nFrame % WINDOW_LENGTH == 0) {
+    // console.log(`nframe = ${nFrame}`);
+    console.log(`ac = ${acdc}`);
+    // console.log(`ac-detrended = ${detrend(acdc)}`);
+    ac = detrend(acdc);
+  }
+  // ac[nFrame % WINDOW_LENGTH] = xMean;
 
-    ctx_tmp.putImageData(frame, 0, 0);
-    setTimeout(computeFrame, delay); // continue with delay
-    // } else {
-    //     pauseVideo();
-    // TODO: send, evaluate, show results
-    // }
+  acFrame = ac[nFrame % WINDOW_LENGTH];
+
+  xMeanArr.push(xMeanData);
+
+  document.getElementById('frame-time').innerHTML = `Frame time: ${xMeanData.time.toFixed(2)}`;
+  document.getElementById('video-time').innerHTML = `Video time: ${(video.currentTime.toFixed(2))}`;
+  document.getElementById('signal').innerHTML = `X: ${xMeanData.x}`;
+
+  const fps = (++frameCount / video.currentTime).toFixed(3);
+  document.getElementById('frame-fps').innerHTML = `Frame count: ${frameCount}, FPS: ${fps}`;
+
+  ctx_tmp.putImageData(frame, 0, 0);
+  setTimeout(computeFrame, delay); // continue with delay
+  nFrame += 1;
+}
+
+function detrend(y) {
+  const n = y.length;
+  let x = [];
+  for (let i = 0; i <= n; i++) {
+    x.push(i);
+  }
+
+  let sx = 0;
+  let sy = 0;
+  let sxy = 0;
+  let sxx = 0;
+  for (let i = 0; i < n; i++) {
+    sx += x[i];
+    sy += y[i];
+    sxy += x[i] * y[i];
+    sxx += x[i] * x[i];
+  }
+  const mx = sx / n;
+  const my = sy / n;
+  const xx = n * sxx - sx * sx;
+  const xy = n * sxy - sx * sy;
+  const slope = xy / xx;
+  const intercept = my - slope * mx;
+
+  detrended = [];
+  for (let i = 0; i < n; i++) {
+    detrended.push(y[i] - (intercept + slope * i));
+  }
+
+  return detrended;
 }
 
 function onRecord() {
-    navigator.mediaDevices.getUserMedia(constraintsObj)
+  this.disabled = true;
+  navigator.mediaDevices.getUserMedia(constraintsObj)
     .then(function(mediaStreamObj) {
 
       // we must turn on the LED / torch
@@ -100,9 +138,9 @@ function onRecord() {
       const photoCapabilities = imageCapture.getPhotoCapabilities()
         .then(() => {
           track.applyConstraints({
-            advanced: [{torch: true}]
-          })
-          .catch(err => console.log('No torch', err));
+              advanced: [{ torch: true }]
+            })
+            .catch(err => console.log('No torch', err));
         })
         .catch(err => console.log('No torch', err));
 
@@ -112,14 +150,14 @@ function onRecord() {
       }
 
       if ("srcObject" in video) {
-          video.srcObject = mediaStreamObj;
+        video.srcObject = mediaStreamObj;
       } else {
-          // for older versions of browsers
-          video.src = window.URL.createObjectURL(mediaStreamObj);
+        // for older versions of browsers
+        video.src = window.URL.createObjectURL(mediaStreamObj);
       }
 
       video.onloadedmetadata = function(ev) {
-          video.play();
+        video.play();
       };
 
       init();
@@ -128,34 +166,25 @@ function onRecord() {
       video.addEventListener('play', drawLineChart);
 
       video.onpause = function() {
-          console.log('paused');
+        console.log('paused');
       };
     })
     .catch(error => console.log(error));
 }
 
 function pauseVideo() {
-    video.pause();
-    video.currentTime = 0;
-}
-
-// draw the signal data as it comes
-let lineArr = [];
-let MAX_LENGTH = 100;
-let duration = 100;
-let chart = realTimeLineChart();
-
-function randomNumberBounds(min, max) {
-  return Math.floor(Math.random() * max) + min;
+  video.pause();
+  video.currentTime = 0;
 }
 
 // start with dummy flat signal
 function seedData() {
   let now = new Date();
+
   for (let i = 0; i < MAX_LENGTH; ++i) {
     lineArr.push({
-      time: new Date(now.getTime() - ((MAX_LENGTH - i) * duration)),
-      x: 1.5 - 250.0/255,
+      time: new Date(now.getTime() - initTime - ((MAX_LENGTH - i) * DURATION)),
+      x: 0.008
     });
   }
 }
@@ -164,8 +193,8 @@ function updateData() {
   let now = new Date();
 
   let lineData = {
-    time: now,
-    x: rgbMean[channel],
+    time: now - initTime,
+    x: acFrame
   };
   lineArr.push(lineData);
 
@@ -184,6 +213,8 @@ function resize() {
 }
 
 function drawLineChart() {
+  initTime = new Date();
+
   seedData();
   window.setInterval(updateData, 100);
   d3.select("#chart").datum(lineArr).call(chart);
